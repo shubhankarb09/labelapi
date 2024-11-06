92ac48e297eb2d1c20f160b7669e259ac12973b1
Shubhankar Bhattarai <bhattaraishubhankar@gmail.com>
2024-11-06 11:57:39 +0100

    validation: type of Post Request

--- microservice/main.py
+++ microservice/main.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 from model import probability_generator
 
 app = FastAPI()
 
 #Input schema
 class PostRequest(BaseModel):
-    caption_text: str
+    caption_text: str = Field(..., min_length=1, description="Caption must be a non-empty string.")
+
+    """Avoiding conversion of caption to type string through coersion.
+    Validator is run before coercion to string is done.
+    """ 
+    @validator("caption_text", pre=True)
+    def check_string(cls, value):
+        if not isinstance(value, str):
+            raise ValueError("caption_text must be a string.")
+        return value
 
 class PostResponse(BaseModel):
     topic_probabilities: dict
 
 @app.post("/predict-topic", response_model=PostResponse)
 async def predict_topic(request: PostRequest):
-    if not request.caption_text:
-        raise HTTPException(status_code=400, detail="The caption cannot be empty")
     
     probabilities = probability_generator(request.caption_text)
     return PostResponse(topic_probabilities=probabilities)
